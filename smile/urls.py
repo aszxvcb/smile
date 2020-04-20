f02d0a9c8c3ba41483cf5e2bccbec4068b40c371
jiwon_lala <s26788761@naver.com>
2020-04-21 01:57:33 +0900

    feat : urls.py에 include 사용
    smile/urls.py 의 gallery app의 url 부분 include 사용하여 정리
    gallery/urls.py 새로 생성 & gallery app의 url 정의

--- smile/urls.py
+++ smile/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 import gallery.views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', gallery.views.home, name="home"),
-    path('gallery', gallery.views.gallery, name="gallery"),
+    path('gallery/', include('gallery.urls')),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
