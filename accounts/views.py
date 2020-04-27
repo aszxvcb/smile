770ec406fbee88016d4d6f3a872fae98762f9a27
jiwon_lala <s26788761@naver.com>
2020-04-27 17:28:07 +0900

    feat : accounts app 구현
    .gitignore에 ngrok 추가(develop-form)
    views.py signup 함수 error 처리를 위한 try-catch 문 추가
    views.py logout 함수 template 위치 수정
    login.html error 부분 추가
    signup.html error 부분 추가

--- accounts/views.py
+++ accounts/views.py
@@ -6,11 +6,19 @@ from django.contrib import auth
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                request.POST['username'], password=request.POST['password1'])
-            auth.login(request. user)
-            return redirect('')
-    return render(request, 'signup.html')
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'signup.html', {'error': "이미 사용된 username입니다"})
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    request.POST['username'], 
+                    password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else :
+            return render(request, 'signup.html' , {'error': "password가 일치하지 않습니다"})
+    else :
+        return render(request, 'signup.html')
     
     
 def login(request):
@@ -35,4 +43,4 @@ def logout(request):
     if request.method == 'POST':
         auth.logout(request)
         return redirect('home')
-    return render(request, 'accounts/signup.html')
+    return render(request, 'signup.html')
