261f9b9aa9ed6ed8496504363ea821394bc55270
jiwon_lala <s26788761@naver.com>
2020-04-30 23:46:59 +0900

    feat : upload 된 Photo class에 임의의 hash값 붙는 문제 해결
    models.py에 unique_file_name 함수 추가하여 "파일명_생성시간" 양식으로 파일명 설정
    	-> 추후에 "str(instance.created_date)"부분 시분초 정보만 사용하는 방향으로 수정필요
    
    gallery.html에 출력되는 Photo class 정보에 "photo.image.url", "photo.image.instance" "photo.image.name" 추가

--- gallery/models.py
+++ gallery/models.py
@@ -5,9 +5,15 @@ from django.conf import settings
 
 
 # Create your models here.
+
+def unique_file_name(instance, filename):
+    return 'images/{0}_{1}'.format(filename, str(instance.created_date))
+
 class Photo(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/')
+    # image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to= unique_file_name)
+
     created_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
