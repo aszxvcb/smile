523a09fdfe8b730f64267b0ec320f0b14f37bd7a
jiwon_lala <s26788761@naver.com>
2020-05-01 15:36:34 +0900

    #9  Fix unique_file_name & Selfie class to support Selife class upload
    models.py의 unique_file_name 함수를 호출한 class가 Photo 인 경우, Selfie 인 경우를 나누어 저장될 루트 리턴하게 수정
    	(Photo class :  "파일명_업로드시간정보.확장자" 형식 / Selfie class :  "사용자이름/파일명_업로드시간정보.확장자" 형식)
    
    	Selfie class created_date의 옵션을 "default=timezone.now"로 수정
    
    참고한 문서 : https://brunch.co.kr/@ddangdol/4

--- gallery/models.py
+++ gallery/models.py
@@ -8,10 +8,17 @@ import os
 # Create your models here.
 
 def unique_file_name(instance, filename):
-    current_time = instance.created_date.today()
+    current_time = instance.created_date
     time_info  = current_time.strftime('%y%m%d%H%M%S')
     root_ext = os.path.splitext(filename)   #filename "file.jpg"에서 "." 기준으로 잘라서 "file" ".jpg"과 같이 나누어 root_ext에 저장
-    return 'images/{0}_{1}{2}'.format(root_ext[0], time_info, root_ext[1])
+
+    instance_type = type(instance)
+    if instance_type is Photo:        
+        return 'images/{0}_{1}{2}'.format(root_ext[0], time_info, root_ext[1])
+    elif instance_type is Selfie:
+        user_name = instance.owner.username
+        return 'images/{0}/{1}_{2}{3}'.format(user_name, root_ext[0], time_info, root_ext[1])
+
 
 class Photo(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -28,9 +35,9 @@ class Photo(models.Model):
 class Selfie(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=unique_file_name)
-    created_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        current_time = self.created_date.today()
+        current_time = self.created_date
         time_info  = current_time.strftime('%y%m%d%H%M%S')
-        return "Selfie_" + self.owner.usernmae + "_" + time_info
+        return "selfie_" + self.owner.username + "_" + time_info
